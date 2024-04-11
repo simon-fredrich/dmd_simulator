f5aa672cfded3ed6cb91ea7e76bfbb399d3965a9
simon-fredrich <simon-fredrich@gmx.de>
2024-04-11 20:57:54 +0200

    first draft of method for visualisation of dmd

--- dmd.py
+++ dmd.py
@@ -1,5 +1,7 @@
 import numpy as np
 from mirror import Mirror
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class DMD:
     '''
@@ -15,10 +17,10 @@ class DMD:
         self.mirror_nr_y = mirror_nr_y # amount of mirrors in the y direction
         self.mirror_width = mirror_width # width of the mirrors
         self.mirror_height = mirror_height # height of the mirrors
-        self.gap_x = gap_x
-        self.gap_y = gap_y
-        self.dmd_width = mirror_nr_x * (mirror_width + gap_x) - gap_x
-        self.dmd_height = mirror_nr_y * (mirror_height + gap_y) - gap_y
+        self.gap_x = gap_x # gap between mirrors in the x direction
+        self.gap_y = gap_y # gap between mirrors in the y direction
+        self.dmd_width = mirror_nr_x * (mirror_width + gap_x) - gap_x # total width of the dmd (x direction)
+        self.dmd_height = mirror_nr_y * (mirror_height + gap_y) - gap_y # total height of the dmd (y direction)
 
     def range_check(self, mirror_x, mirror_y, tilt_angle, s, t):
         '''
@@ -68,18 +70,57 @@ class DMD:
         sin = np.sin(gamma)
         matrix_product = 1/np.sqrt(2) * (- s * sin + t * sin)
         return matrix_product
+    
+    def get_coordinates(self, mirror_nr_x, mirror_nr_y, tilt_angle, s, t):
+        x = self.get_x(mirror_nr_x, mirror_nr_y, tilt_angle, s, t)
+        y = self.get_y(mirror_nr_x, mirror_nr_y, tilt_angle, s, t)
+        z = self.get_z(mirror_nr_x, mirror_nr_y, tilt_angle, s, t)
+        return np.array([x, y, z])
+    
+    def show_surface(self, tilt_angle):
+        '''
+        Display surface of the dmd where all mirrors are in the 
+        same state.
+        '''
+        vals_per_mirror = 4
+        width = int(self.mirror_nr_x * (self.mirror_width + self.gap_x))
+        height = int(self.mirror_nr_y * (self.mirror_height + self.gap_y))
+        print(width, height)
+        print(self.dmd_width, self.dmd_height)
+        print(self.get_coordinates(0, 0, 12, 3, 4))
+        surface = np.zeros((width, height), np.double)
+        x_values = np.array([])
+        y_values = np.array([])
+        z_values = np.array([])
+
+        for m_x in np.arange(self.mirror_nr_x):
+            for m_y in np.arange(self.mirror_nr_y):
+                for s in np.arange(vals_per_mirror):
+                    s_i = self.mirror_width / vals_per_mirror * s
+                    for t in np.arange(vals_per_mirror):
+                        t_i = self.mirror_height / vals_per_mirror * t
+                        x = self.get_x(m_x, m_y, tilt_angle, s_i, t_i)
+                        x_values = np.append(x_values, x)
+                        y = self.get_y(m_x, m_y, tilt_angle, s_i, t_i)
+                        y_values = np.append(y_values, y)
+                        z = self.get_z(m_x, m_y, tilt_angle, s_i, t_i)
+                        z_values = np.append(z_values, z)
+                        # print(x, y, z)
+
+        plt.scatter(x_values, y_values, c=z_values, cmap='viridis', s=20)
+        plt.colorbar(label='Z values')
+        plt.show()
+
+
 
 def main():
-    nrY = 1080
-    nrX = 1920
-    latticeConstant = 7.56
-    fillFactor = 0.92
-    mirrorSize = np.sqrt(latticeConstant*latticeConstant*fillFactor)
-    gap = latticeConstant-mirrorSize
-    dmd = DMD(nrX, nrY, mirrorSize, mirrorSize, gap, gap)
-    print(dmd.get_x(0, 0, 40, 2, 2))
-    print(dmd.get_y(0, 0, 40, 2, 2))
-    print(dmd.get_z(0, 0, 40, 2, 2))
+    mirror_nr_x = 6
+    mirror_nr_y = 6
+    mirror_width = 5
+    mirror_height = 5
+    gap = 0.5
+    dmd = DMD(mirror_nr_x, mirror_nr_y, mirror_width, mirror_height, gap, gap)
+    dmd.show_surface(-45)
 
 if __name__ == "__main__":
     main()
